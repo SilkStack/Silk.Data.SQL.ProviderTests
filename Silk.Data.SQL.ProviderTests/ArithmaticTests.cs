f65e13949783085ebb3d6f90213c393cc104d8e4
John Carruthers <johnc@frag-labs.com>
2018-08-14 11:45:41 -0500

    Add tests for string concatenation and basic integer math.

---
+++ Silk.Data.SQL.ProviderTests/ArithmaticTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Silk.Data.SQL.Expressions;
+using System.Threading.Tasks;
+
+namespace Silk.Data.SQL.ProviderTests
+{
+	public partial class SqlProviderTests
+	{
+		[TestMethod]
+		public async Task Arithmatic_Addition()
+		{
+			using (var queryResult = await DataProvider.ExecuteReaderAsync(
+				QueryExpression.Select(QueryExpression.Add(
+					QueryExpression.Value(1), QueryExpression.Value(2)
+					))
+				))
+			{
+				Assert.IsTrue(queryResult.HasRows);
+				Assert.IsTrue(await queryResult.ReadAsync());
+				Assert.AreEqual(3, queryResult.GetInt32(0));
+			}
+		}
+
+		[TestMethod]
+		public async Task Arithmatic_Subtraction()
+		{
+			using (var queryResult = await DataProvider.ExecuteReaderAsync(
+				QueryExpression.Select(QueryExpression.Subtract(
+					QueryExpression.Value(3), QueryExpression.Value(2)
+					))
+				))
+			{
+				Assert.IsTrue(queryResult.HasRows);
+				Assert.IsTrue(await queryResult.ReadAsync());
+				Assert.AreEqual(1, queryResult.GetInt32(0));
+			}
+		}
+
+		[TestMethod]
+		public async Task Arithmatic_Multiply()
+		{
+			using (var queryResult = await DataProvider.ExecuteReaderAsync(
+				QueryExpression.Select(QueryExpression.Multiply(
+					QueryExpression.Value(2), QueryExpression.Value(3)
+					))
+				))
+			{
+				Assert.IsTrue(queryResult.HasRows);
+				Assert.IsTrue(await queryResult.ReadAsync());
+				Assert.AreEqual(6, queryResult.GetInt32(0));
+			}
+		}
+
+		[TestMethod]
+		public async Task Arithmatic_Divide()
+		{
+			using (var queryResult = await DataProvider.ExecuteReaderAsync(
+				QueryExpression.Select(QueryExpression.Divide(
+					QueryExpression.Value(6), QueryExpression.Value(2)
+					))
+				))
+			{
+				Assert.IsTrue(queryResult.HasRows);
+				Assert.IsTrue(await queryResult.ReadAsync());
+				Assert.AreEqual(3, queryResult.GetInt32(0));
+			}
+		}
+	}
+}
---
+++ Silk.Data.SQL.ProviderTests/ConcatTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Silk.Data.SQL.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Silk.Data.SQL.ProviderTests
+{
+	public partial class SqlProviderTests
+	{
+		[TestMethod]
+		public virtual async Task Concat_SelectStrings()
+		{
+			using (var queryResult = await DataProvider.ExecuteReaderAsync(
+				QueryExpression.Select(QueryExpression.Concat(
+					QueryExpression.Value("Hello"), QueryExpression.Value(" "), QueryExpression.Value("World")
+					))
+				))
+			{
+				Assert.IsTrue(queryResult.HasRows);
+				Assert.IsTrue(await queryResult.ReadAsync());
+				Assert.AreEqual("Hello World", queryResult.GetString(0));
+			}
+		}
+	}
+}
